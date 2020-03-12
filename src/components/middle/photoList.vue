3701125e5aa2b241b5c41227d96a4420a44a3d37
v-yui <1356674019@qq.com>
2020-03-12 22:02:44 +0800

    增加主页图片模块

--- src/components/middle/photoList.vue
+++ src/components/middle/photoList.vue
@@ -1,40 +1,57 @@
 <template>
     <div>
         <!--mui的顶部滑动条-->
-<!--        <div id="slider" class="mui-slider mui-fullscreen">-->
             <div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
                 <div class="mui-scroll">
-                    <a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
-                        推荐
-                    </a>
-                    <a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
-                        热点
-                    </a>
-                    <a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
-                        北京
-                    </a>
-                    <a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
-                        社会
-                    </a>
-                    <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-                        娱乐
-                    </a>
-                    <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-                        科技
+                    <!--在ｃｌａｓｓ里使用三元表达式设置第一项激活ｍｕｉ－ａｃｔｉｖｅ高亮(其他选项的高亮是滑动组件内的)-->
+                    <a :class="['mui-control-item', item.id == 0 ? 'mui-active' : '']" v-for="item in imgCategory" :key="item.id">
+                        {{item.title}}
                     </a>
                 </div>
             </div>
 
         </div>
-<!--    </div>-->
 </template>
 
 <script>
+    //mui的滑动控件
+    import mui from '../../lib/mui/js/mui.min.js';
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005//flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
     export default {
-        name: "photoList"
+        name: "photoList",
+        data() {
+            return {
+                imgCategory: []
+            }
+        },
+        mounted() {
+            mui('.mui-scroll-wrapper').scroll({
+                deceleration: 0.0005//flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+            });
+        },
+        created() {
+            this.getAllCategory()
+        },
+        methods: {
+            getAllCategory() {
+                this.$http.get('api/getimgcategory').then(result => {
+                    if(result.body.status === 0) {
+                        //全部分类需要手动拼接出来
+                        result.body.message.unshift({title: "全部", id: 0})
+                        this.imgCategory = result.body.message
+                    }
+                })
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    * {
+        touch-action: pan-y
+        /* touch-action 用于设置触摸屏用户如何操纵元素的区域(例如，浏览器内置的缩放功能)。*/
+    /*避免ｃｈｒｏｍｅ报错*/
+    }
 </style>
--- src/gate.vue
+++ src/gate.vue
@@ -8,20 +8,20 @@
         </transition>
         <!--mui 的bar-tab(含路由-->
         <nav class="mui-bar mui-bar-tab">
-            <router-link class="mui-tab-item" to="/home">
+            <router-link class="mui-tab-item1" to="/home">
                 <span class="mui-icon mui-icon-home"></span>
                 <span class="mui-tab-label">首页</span>
             </router-link>
-            <router-link class="mui-tab-item" to="/contact">
+            <router-link class="mui-tab-item1" to="/contact">
                 <span class="mui-icon mui-icon-contact"></span>
                 <span class="mui-tab-label">会员</span>
             </router-link>
-            <router-link class="mui-tab-item" to="cart">
+            <router-link class="mui-tab-item1" to="cart">
                 <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
                     <span class="mui-badge">0</span></span>
                 <span class="mui-tab-label">购物车</span>
             </router-link>
-            <router-link class="mui-tab-item" to="search">
+            <router-link class="mui-tab-item1" to="search">
                 <span class="mui-icon mui-icon-search"></span>
                 <span class="mui-tab-label">搜索</span>
             </router-link>
@@ -54,4 +54,33 @@
     .v-leave-active {
         transition: all 0.5s ease;
     }
+    /*写了图片的滑动栏，与ｂａｒ-tab冲突，重写mui-tab-item相关样式*/
+    .mui-bar-tab .mui-tab-item1.mui-active {
+        color: #007aff;
+    }
+    .mui-bar-tab .mui-tab-item1 {
+      display: table-cell;
+      overflow: hidden;
+      width: 1%;
+      height: 50px;
+      text-align: center;
+      vertical-align: middle;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      color: #929292;
+    }
+    .mui-bar-tab .mui-tab-item1 .mui-icon {
+        top: 3px;
+        width: 24px;
+        height: 24px;
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+    .mui-bar-tab .mui-tab-item1 .mui-icon~.mui-tab-label {
+        font-size: 11px;
+        display: block;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
 </style>
